b3a6151e9c23d34a135781cd38f1d43c494453f4
SharathHebbar <Sharathhebbar24@gmail.com>
2021-11-20 13:50:15 +0530

    updates

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import *
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+from sklearn.metrics import accuracy_score
 
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
@@ -17,9 +21,36 @@ def home():
 def detect():
     if request.method == 'POST':
         inp = str(request.form.values())
+        data = pd.read_csv(
+            'C:\\projects\ml\\SMS-spam-detection-using-machine-learning\\spam.csv', encoding='latin-1')
+        data = data.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
+        data = data.rename(columns={'v1': 'result'})
+        data = data.rename(columns={'v2': 'messages'})
+        Y = np.array(data['result'])
+        X = np.array(data['messages'])
+        x = cv.fit_transform(X)
+        best = 0
+        for i in range(100):
+            xtrain, xtest, ytrain, ytest = train_test_split(x, Y)
+            clf = MultinomialNB()
+            clf.fit(xtrain, ytrain)
+            yp = clf.predict(xtest)
+            acc = accuracy_score(ytest, yp)
+            if(acc > best):
+                best = acc
+                with open("spam.pickle", "wb") as f:
+                    pickle.dump(clf, f)
+
         res = cv.transform([inp]).toarray()
-        clf = pickle.load(open('spam.pickle', 'rb'))
+        clf = pickle.load(open(
+            'C:\\projects\ml\\SMS-spam-detection-using-machine-learning\\spam.pickle', 'rb'))
         op = clf.predict(res)
+        # op = op[0]
+        # print(op)
+        # if op == 'ham':
+        #     op = "The message is not spam"
+        # else:
+        #     op = 'The message is spam'
 
         return render_template('welcome.html', op=op)
 
